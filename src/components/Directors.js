3b881802ce240539282a6bd93822bb4963fccb66
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-21 13:19:43 -0700

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,13 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(d =>
+        <div>
+          <h3>Name: {d.name}</h3>
+          <p>Movies: <ul>{d.movies.map(movie => <li>{movie}</li>)}</ul></p>
+        </div>
+      )}
     </div>
   );
 }
